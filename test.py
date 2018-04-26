dc3febd30ff271a11d0f4d24516a0043dccb9d8e
Marijn <marijn.nijsse@gmail.com>
2018-04-26 18:29:15 +0200

    Create new button button
    Added the possibility to create a new button. These will eventually represent zombies/whatevers

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 #http://thinkingtkinter.sourceforge.net/
-from Tkinter import *
+from tkinter import *
 
 class MyApp:                         
-    def __init__(self, myParent):      
-        self.myContainer1 = Frame(myParent) #Create a container, which stores data but displays nothing
-        self.myContainer1.pack()
-        
-        self.button1 = Button(self.myContainer1, text="Click me!", background="red") #Button stored in myContainer1, a 'widget'
-        self.button1.pack()
-        self.button1.bind("<Button-1>", self.button1Click)               
-        
-    def button1Click(self, event):      
-        self.button1.configure(text="Clicked!")
+	def __init__(self, myParent):      
+		self.myContainer1 = Frame(myParent) #Create a container, which stores data but displays nothing
+		self.myContainer1.pack()
+		
+		self.button1 = Button(self.myContainer1, text="Click me!", background="red") #Button stored in myContainer1, a 'widget'
+		self.button1.pack()
+		self.button1.bind("<Button-1>", self.button1Click)  
+		
+	def button1Click(self, event):      
+		self.button1.configure(text="Clicked!")
+		self.button2 = Button(self.myContainer1, text="The Child" + str(event.x))
+		self.button2.pack(side=LEFT)
+		#self.button2.bind("<Button-1>", lamba e: self.destroy())
 
 root = Tk()
 myApp = MyApp(root)
