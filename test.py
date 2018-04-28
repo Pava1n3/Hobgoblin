fdb3311cc0974873b5efdae9ad08fe058d2bc93f
Marijn <marijn.nijsse@gmail.com>
2018-04-28 11:33:17 +0200

    Cleared up variable names
    No longer tutorial myApp stuff. Added soem container things as well

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
 #http://thinkingtkinter.sourceforge.net/
 from tkinter import *
 
-class MyApp:                         
-	def __init__(self, myParent):      
-		self.myContainer1 = Frame(myParent) #Create a container, which stores data but displays nothing
-		self.myContainer1.pack()
+class HobGoblin:                       
+	creatureArray = []
+	creatureCount = 0		
+  
+	def __init__(self, parent):      
+		self.mainContainer = Frame(parent) #Create a container, which stores data but displays nothing
+		self.mainContainer.pack()
+		self.creatureContainer = Frame(parent)
 		
-		self.button1 = Button(self.myContainer1, text="Click me!", background="red") #Button stored in myContainer1, a 'widget'
-		self.button1.pack()
-		self.button1.bind("<Button-1>", self.button1Click)  
+		self.makeCreatureButton = Button(self.mainContainer, text="Click me!", background="red") #Button stored in mainContainer, a 'widget'
+		self.makeCreatureButton.pack()
+		self.makeCreatureButton.bind("<Button-1>", self.makeCreatureButtonClick)  
 		
-	def button1Click(self, event):      
-		self.button1.configure(text="Clicked!")
-		self.button2 = Button(self.myContainer1, text="The Child" + str(event.x), command= lambda : self.button2.destroy())
+	def makeCreatureButtonClick(self, event):      
+		#self.makeCreatureButton.configure(text="Clicked!")
+		self.button2 = Button(self.mainContainer, text=str(self.creatureCount) + "The Child" + str(event.x), command= lambda : self.button2.destroy())
 		self.button2.bind("<Button-1>", lambda e : self.button2.destroy()) #sketchy af atm
 		self.button2.pack(side=LEFT)
 
 root = Tk()
-myApp = MyApp(root)
+hobGoblin = HobGoblin(root)
 root.wm_attributes("-topmost", 1) 
 root.mainloop()
